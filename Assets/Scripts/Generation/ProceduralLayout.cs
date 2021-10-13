e8d1401978e0c4f33f6507a16b6359c4dde5e112
EricLocher <eriloc1129@skola.goteborg.se>
2021-10-13 17:28:38 +0200

    Working on Generation Step 1
    ++ Room generation in circle
    ++ Collision simulation working (kinda)

--- Assets/Scripts/Generation/ProceduralLayout.cs
+++ Assets/Scripts/Generation/ProceduralLayout.cs
@@ -9,11 +9,12 @@ public class ProceduralLayout : MonoBehaviour
     private List<Room> rooms = new List<Room>();
 
     //TEMP
-    float _SCircleRadius = 3;
+    [SerializeField]
+    private float _SCircleRadius = 3;
 
     private void Awake()
     {
-        
+
     }
 
     private void Start()
@@ -26,8 +27,10 @@ public class ProceduralLayout : MonoBehaviour
         if(rooms.Count > 0)
         foreach (Room room in rooms)
         {
+            room.Collision(rooms);
             room.draw();
         }
+
     }
 
     //TEMP FUNCTION FOR TESTING
@@ -40,8 +43,49 @@ public class ProceduralLayout : MonoBehaviour
     private void GenerateRooms()
     {
 
+        for (int i = 0; i < amountOfRooms; i++)
+        {
+            while (true)
+            {
+
+                float _roomX = Random.Range(transform.position.x - _SCircleRadius, transform.position.x + _SCircleRadius);
+                float _roomY = Random.Range(transform.position.y - _SCircleRadius, transform.position.y + _SCircleRadius);
+
+                float _roomW = generateGaussian((float)0.5, 2, 100);
+                float _roomH = generateGaussian((float)0.5, 2, 100);
+
+                if (Mathf.Pow((_roomX + (_roomW / 2)) - transform.position.x, 2) + Mathf.Pow((_roomY + (_roomH / 2)) - transform.position.y, 2) < Mathf.Pow(_SCircleRadius, 2))
+                {
+                    rooms.Add(new Room(new Vector2(_roomX, _roomY), _roomW, _roomH));
+                    break;
+                }
+
+            }
+
+        }
+
     }
 
 
+
+    private float generateGaussian(float min, float max, float std)
+    {
+        while (true)
+        {
+            float mean = (min + max) / 2;
+            float _2PI = Mathf.PI * 2;
+            float u1 = Random.value;
+            float u2 = Random.value;
+
+            float z0 = Mathf.Sqrt((float)(-2.0 * Mathf.Log(u1))) * Mathf.Cos(_2PI * u2);
+            float z1 = Mathf.Sqrt((float)(-2.0 * Mathf.Log(u1))) * Mathf.Sin(_2PI * u2);
+
+            float retVal = z0 * std + mean;
+            if (retVal > min && retVal < max)
+            {
+                return retVal;
+            }
+        }
+    }
 
 }
--- Assets/Scripts/Generation/Room.cs
+++ Assets/Scripts/Generation/Room.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,15 +7,22 @@ namespace ProceduralLayoutGeneration
     {
         private Vector2 pos;
         private float w, h;
+
         public Room(Vector2 pos, float w, float h)
         {
             this.pos = pos;
             this.w = w;
             this.h = h;
+
+            this.pos.x = Mathf.Round(this.pos.x * 10) / 10;
+            this.pos.y = Mathf.Round(this.pos.y * 10) / 10;
         }
 
         public void draw()
         {
+            this.pos.x = Mathf.Round(this.pos.x * 10) / 10;
+            this.pos.y = Mathf.Round(this.pos.y * 10) / 10;
+            Debug.Log(this.pos.x + " : " + this.pos.y);
 
             Debug.DrawLine(new Vector3(pos.x, pos.y), new Vector3(pos.x + w, pos.y), Color.red);
             Debug.DrawLine(new Vector3(pos.x, pos.y + h), new Vector3(pos.x + w, pos.y + h), Color.red);
@@ -24,6 +30,52 @@ namespace ProceduralLayoutGeneration
             Debug.DrawLine(new Vector3(pos.x + w, pos.y), new Vector3(pos.x + w, pos.y + h), Color.red);
         }
 
+        public void Collision(List<Room> rooms)
+        {
+            foreach (Room room in rooms)
+            {
+                if (this == room) { break; }
+                if
+                (
+                  ((this.pos.x + this.w) > room.pos.x && this.pos.x < (room.pos.x + room.w))
+                  &&
+                  ((this.pos.y + this.h) > room.pos.y && this.pos.y < (room.pos.y + room.h))
+                )
+                {
+                    Vector2 _rect1Center = new Vector2(this.pos.x + (this.w / 2), this.pos.y + (this.h / 2));
+                    Vector2 _rect2Center = new Vector2(room.pos.x + (room.w / 2), room.pos.y + (room.h / 2));
 
+                    float _xDiff = Mathf.Abs(_rect1Center.x - _rect2Center.x);
+                    float _yDiff = Mathf.Abs(_rect1Center.y - _rect2Center.y);
+
+                    if (_xDiff > _yDiff)
+                    {
+                        if (_rect1Center.x < _rect2Center.x)
+                        {
+                            this.pos.x -= (float)0.1;
+                            room.pos.x += (float)0.1;
+                        }
+                        else
+                        {
+                            this.pos.x += (float)0.1;
+                            room.pos.x -= (float)0.1;
+                        }
+                    }
+                    else
+                    {
+                        if (_rect1Center.y < _rect2Center.y)
+                        {
+                            this.pos.y -= (float)0.1;
+                            room.pos.y += (float)0.1;
+                        }
+                        else
+                        {
+                            this.pos.y += (float)0.1;
+                            room.pos.y -= (float)0.1;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
